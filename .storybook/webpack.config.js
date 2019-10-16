045db8b0881464625c1d021b90f538519da59a5e
Daniel Scanlon <dscanlonpa@gmail.com>
2019-10-16 16:45:00 -0400

    Simplify webpack config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,19 +1,12 @@
 module.exports = ({ config }) => {
 	config.module.rules.push({
-		test: /\.(ts|tsx)$/,
+		test: /\.tsx?$/,
 		use: [
-			{
-				loader: require.resolve("awesome-typescript-loader"),
-				options: {
-					presets: [["react-app", { flow: false, typescript: true }]],
-					configFileName: "./.storybook/tsconfig.json"
-				}
-			},
-			{
-				loader: require.resolve("react-docgen-typescript-loader")
-			}
-		]
-	})
-	config.resolve.extensions.push(".ts", ".tsx")
-	return config
+			{ loader: require.resolve("awesome-typescript-loader") },
+		],
+	});
+
+	config.resolve.extensions.push(".ts", ".tsx");
+
+	return config;
 }
