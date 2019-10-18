be80c5a059ee08704922a6dddaaf1a343a82bdec
Daniel Scanlon <dscanlonpa@gmail.com>
2019-10-18 11:38:23 -0400

    Corrected a comment

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,7 +2,7 @@ import { withInfo } from '@storybook/addon-info';
 import { addDecorator, configure } from '@storybook/react';
 import './example-base.css';
 
-// Automatically import all files ending in *.stories.js
+// Automatically import all files ending in *.stories.tsx
 configure(require.context('./stories', true, /\.stories\.tsx$/), module);
 
 // Add info decorator
